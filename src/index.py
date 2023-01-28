425822de8db5ffeaa52590625bb3b8076e3b43c5
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2023-01-28 11:11:31 +0200

    src/index can now be executed outside src :p

--- src/index.py
+++ src/index.py
@@ -1,26 +1,33 @@
 from time import time
+from pathlib import Path
 from constants import init
 from trie import Node, Trie
 from markov_process import Markov
 
 
 def main():
-    path = '../data/'
+    path = 'data/'
     file = input('Syötä tiedoston nimi: ')
     start_str = input('Syötä lauseen aloittava sana: ')
+
+    if 'src' in str(Path('.').resolve()):
+        path = '../data/'
     if not file:
         file = 'viehattava.txt'
-    init(path+file)
+
+    filepath = Path(path+file).resolve()
+    init(filepath)
     a = time()
     # print(sentences)
     # print(ALPHABET)
     trie = Trie(Node())
     trie.fill_with_sentences()
-    print(trie)
+    #print(trie)
     mark = Markov(trie)
     s = mark.create_sentence_random(start_str or 'Minä')
     if not s:
         s = 'Kokeile eri sanaa'
+    
     print('\n'+s+'\n')
     b = time()
     print(b-a, 's')
